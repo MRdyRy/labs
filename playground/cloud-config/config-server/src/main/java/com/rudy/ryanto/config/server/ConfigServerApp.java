8d13f2d6448d2d19eb981f9d93518422645fe3ad
MRdyRy <ryanserfanru@gmail.com>
2023-07-01 01:06:02 +0700

    update add cloud-config

---
+++ playground/cloud-config/config-server/src/main/java/com/rudy/ryanto/config/server/ConfigServerApp.java
@@ -0,0 +1,9 @@
+package com.rudy.ryanto.config.server;
+
+import org.springframework.boot.SpringApplication;
+
+public class ConfigServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class,args);
+    }
+}
